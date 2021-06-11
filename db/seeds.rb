74730454b82f2a55136c7fccdb873b32b1ce9fa7
Eliza Plowden <elizaplowden@gmail.com>
2021-06-11 11:17:36 +0100

    seeding with data from api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning up database"
+Quote.destroy_all
+puts "Database cleaned"
+
+url = "https://zenquotes.io/api/random"
+
+quote_serialized = open(url).read
+quotes = JSON.parse(quote_serialized)
+quotes.each do |quote|
+  puts "Creating quote"
+  Quote.create(
+    quote: quote["q"],
+    author:  quote["a"]
+    )
+end
+
+puts "Quotes created"
