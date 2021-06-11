4e319a395a354c3e9d566e9b2df9abff2713e4c1
Eliza Plowden <elizaplowden@gmail.com>
2021-06-11 11:20:02 +0100

    new and create actions. New form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,13 @@ class QuotesController < ApplicationController
   def index
     @quotes = Quote.all
   end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(params[:quote])
+    @quote.save
+  end
 end
