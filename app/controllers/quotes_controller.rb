880a60063b5597c8c395377f59315d72fc4136b8
Eliza Plowden <elizaplowden@gmail.com>
2021-06-11 12:00:57 +0100

    destroy action

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,10 +1,11 @@
 class QuotesController < ApplicationController
+  before_action :find_quote, only: [:show, :destroy]
+
   def index
     @quotes = Quote.all
   end
 
   def show
-    @quote = Quote.find(params[:id])
   end
 
   def new
@@ -18,9 +19,19 @@ class QuotesController < ApplicationController
     redirect_to quote_path(@quote)
   end
 
+  def destroy
+    @quote.destroy
+
+    redirect_to quotes_path
+  end
+
   private
 
   def strong_params
     params.require(:quote).permit(:quote, :author)
+  end
+
+  def find_quote
+    @quote = Quote.find(params[:id])
   end
 end
